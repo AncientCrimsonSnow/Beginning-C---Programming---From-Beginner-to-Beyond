663cfd601e0e41e158c4a8fa4b2fc05dd001247f
Juri Wiechmann <43830164+AncientCrimsonSnow@users.noreply.github.com>
2025-07-26 00:13:51 +0200

    34

--- Workspace01/FirstProgramm/main.cpp
+++ Workspace01/FirstProgramm/main.cpp
@@ -7,6 +7,7 @@ int main(){
     std::cout << "Enter your favorite number between 1 and 100: ";
     std::cin >> favoriteNumber;
     std::cout << "Amating!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favoriteNumber << " is my favorite number!";
     return 0;
 }
 
